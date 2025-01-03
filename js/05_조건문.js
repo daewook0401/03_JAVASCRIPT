971ebe67e96f6dd2cc37b044bdd86a1770ebe2b9
daewook0401 <daewook@gimdaeug-ui-MacBookPro.local>
2025-01-03 17:23:46 +0900

    pm2

---
+++ js/05_조건문.js
@@ -0,0 +1,75 @@
+/** if 예제 - 양수인지 검사 */
+function check1(){
+
+  // id가 "input1"인 요소를 얻어와 in1 상수에 저장
+
+  const in1 = document.getElementById("input1");
+
+  //id가 "input1"인 요소에 작성된 값을 얻어와 숫자로 변환하여
+  // val 변수에 저장
+  const val = Number(in1.value);
+
+  //양수가 맞는지 검사
+  if(val>0)
+  {
+    //조건식이 true인 경우 수행할 코드;
+    alert(`${val}은 양수 입니다.`); //브라우저 알림이 뜸
+  }
+  if(val<=0){
+    alert(`${val}은 양수가 아닙니다.`);
+  }
+}
+
+function check2(){
+  //난수 발생 : Math.random() 0<= 랜덤 <1;
+  //Math.floow는 내림처리 
+  const randomNumber = Math.floor(Math.random()*100+1)
+  if (randomNumber % 2 === 0)
+  {
+    alert(`${randomNumber} 짝수`)
+  }
+  else
+  {
+    alert(`${randomNumber} 홀수`)
+  }
+}
+
+function check3(){
+  const input3 = document.getElementById("input3");
+  const val3 = Number(input3.value);
+
+  let result; //undefined
+  
+  if (val3>0){
+    result = "양수";
+  }
+  else if (val3<0){
+    result = "음수";
+  }
+  else {
+    result = "0";
+  }
+
+  alert(`${val3}은/는 ${result} 입니다`)
+}
+
+function ageCheck(){
+  const inputAge = document.getElementById("inputAge");
+  const Age = Number(inputAge.value);
+
+  let result;
+  if (Age<0 || Age>120){
+    result = '범위를 초과합니다. 0이상 120이하의 나이를 입력하십시오'
+  }
+  else if (Age>=0 && Age <14){
+    result = '어린이입니다.'
+  }
+  else if (Age>=14 && Age <20){
+    result = '청소년입니다.'
+  }
+  else if (Age>=20 && Age <121){
+    result = '성인입니다.'
+  }
+
+  alert(`${Age}은/는 ${result}`)
+}
