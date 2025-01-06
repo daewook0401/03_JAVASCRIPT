242c0825c6e5fb40fabd3aa7f36cbbfc94d437da
daewook0401 <daewook@gimdaeug-ui-MacBookPro.local>
2025-01-06 12:43:13 +0900

    Am

--- js/05_조건문.js
+++ js/05_조건문.js
@@ -96,3 +96,117 @@ function ageCheck2(){
   }
   alert(`${Age}은/는 ${result}`)
 }
+
+/** 두 수 A, B를 입력 받아 A가 B의 배수가 맞는지 확인 */
+function check4(){
+
+  // id가 inputA, inputB인 요소를 얻어와 변수에 저장
+  const inputA = document.getElementById("inputA");
+  const inputB = document.getElementById("inputB");
+  
+  if(inputA.value.length === 0 || inputB.value.length === 0){
+    alert("입력되지 않은 값이 존재합니다.");
+    return; //함수를 즉시 종료하고 호출한 곳으로 돌아감
+            // -> 함수를 수행할 수 없는 상태여서 미리 종료
+  }
+  
+  // 위 if문을 건너 뜀 == 둘다 입력된 상태
+
+  const v1 = Number(inputA.value);
+  const v2 = Number(inputB.value);
+
+  if (v1<v2){
+    alert("A가 B보다 작습니다.")
+    return;
+  }
+
+  if(v1 % v2 === 0){ //A가 B의 배수가 맞을 경우
+    alert(`${v1}은 ${v2}의 배수가 맞습니다.`);
+    return;
+  }
+  alert(`${v1}은 ${v2}의 배수가 아닙니다.`);
+}
+
+function check5(){
+  const input5 = document.getElementById("input5");
+
+  if(input5.value.length === 0){
+    alert("숫자를 입력해주세요");
+    return;
+  }
+  let result;
+  switch( Number(input5.value)){
+    case 3 : result = "3이 입력 되었습니다."; break;
+    case 4 : result = "입력된 숫자는 4 입니다."; break;
+    case 5 : result = "5를 입력하셨습니다"; break;
+    default : result = "3,4,5가 아닌 다른 숫자가 입력 되었습니다.";
+  }
+  alert(result);
+  
+  /* switch문 -> if else문으로 변경 */
+
+  let result2;
+  if (Number(input5.value)===3){
+    result2 = "3이 입력 되었습니다.";
+  }else if(Number(input5.value)===4){
+    result2 = "입력된 숫자는 4 입니다.";
+  }else if(Number(input5.value)===5){
+    result2 = "5를 입력하셨습니다.";
+  }else{
+    result2 = "3,4,5가 아닌 다른 숫자가 입력 되었습니다."
+  }
+  alert(result2);
+}
+
+function calc(Op){
+  const number1 = document.getElementById("number1");
+  const number2 = document.getElementById("number2");
+  if (number1.value.length === 0 || number2.value.length === 0){
+    alert("숫자를 입력해주세요.");
+    return;
+  }
+  const n1 = Number(number1.value);
+  const n2 = Number(number2.value);
+  let result;
+  switch (Op) {
+    case '+' : result = n1 + n2; break;
+    case '-' : result = n1 - n2; break;
+    case '*' : result = n1 * n2; break;
+    case '/' : result = n1 / n2; break;
+    case '%' : result = n1 % n2; break;
+    default : result = "잘못된 연산자";
+  }
+  document.getElementById("calcResult").innerText = result;
+}
+
+function check6(){
+  const input6 = document.getElementById("input6");
+
+  if (input6.value.length === 0){
+    alert("월(달)을 입력해 주세요.")
+    return;
+  }
+
+  // break(멈추다) : 현재 case 까지만 해석하고 switch문을 멈춤
+
+  // -> break 미작성 시
+  // 현재 case 수행 후 멈추지 않고
+  // 다음 case를 연달아서 수행
+  let result;
+  switch(Number(input6.value)){
+    case 3 : case 4 : case 5 : result = "봄"; break;
+    case 6 : case 7 : case 8 : result = "여름"; break;
+    case 9 : case 10 :  case 11 : result = "가을"; break;
+    case 12 : case 1 : case 2 : result = "겨울"; break;
+    default : result = "잘못 입력";
+  }
+
+  alert(result);
+}
+
+/* 14 / 3 = 4
+   9 / 3 = 3
+   6 / 3 = 2
+  3 / 3 = 1
+
+*/
---
+++ js/06_입출금하기.js
@@ -0,0 +1,79 @@
+/* 전역 변수(JS 전체에서 공용으로 사용할 수 있는 변수) */
+
+// 잔액이 출력 되는 span 요소
+const output = document.getElementById("output");
+
+//금액이 입력되는 input 요소
+const amoumt = document.getElementById("amount");
+
+// 잔액을 기록할 변수(초기값 10000)
+let balance = 10000;
+
+// 비밀번호 저장 변수( 초기값 : "1234")
+const password = "1234"; 
+
+// ---------------------------------------------------------------
+
+// 함수 내부에 작성되지 않은 코드
+// -> HTML 페이지 로딩(랜더링) 시
+
+
+output.innerText = balance;
+
+amount.value=""; // input에 작성된 값을 삭제하는 효과
+// -> 입, 출금 함수 마지막에 사용하세요.
+
+function test(){
+  // prompt(내용)
+  // - 알림창에 값을 입력할 수 있는 형태
+
+  // - 확인 클릭 : 입력 된 값 반환
+  // - 취소 클릭 : null qksghks
+  
+  const pw = prompt("비밀번호를 입력하세요.");
+
+  console.log("pw : ", pw);
+
+  if (pw === null){
+    alert("비밀번호 입력 취소됨");
+    return;
+  }
+
+  if(pw !== password){
+    alert("비밀번호가 일치하지 않습니다");
+  return;
+  }
+  alert("출금 기능 수행");
+
+}
+
+/**입금 */
+function deposit(){
+  const Num = Number(amount.value);
+  if (Num <= 0 || isNaN(Num)){
+    alert("금액 입력이 잘못되었습니다.");
+    return;
+  }
+  balance+=Num;
+  output.innerText=balance;
+}
+
+/**출금 */
+function withdrawal(){
+  const Num = Number(amount.value);
+  if (Num <= 0 || isNaN(Num)){
+    alert("금액 입력이 잘못되었습니다.");
+    return;
+  }
+  if (prompt("비밀번호 입력")!==password){
+    alert("비밀번호가 일치하지 않습니다");
+    return;
+  }
+  if (Num > balance){
+    alert("출금 금액이 잔액보다 클 수 없습니다");
+    return;
+  }
+  balance-=Num;
+  output.innerText=balance;
+  alert(`${Num}원이 출금 되었습니다. 남은 잔액 : ${balance}원`)
+}
